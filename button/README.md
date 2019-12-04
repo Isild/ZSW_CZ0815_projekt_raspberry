7a6ee7f00c654dbb67d3c374914976e8fd0adfd3
lpawelcz <czarnecki.l.p@gmail.com>
2019-12-04 22:31:41 +0000

    fix

--- button/README.md
+++ button/README.md
@@ -1,5 +1,12 @@
 # Definition of button service
 
+## Variables
+In button.py there are 3 variables:
+* `BUTTONPIN` for specifying pin for a button
+* `mountpoint` is path to place where the flash drive will be mounted
+* `copydir` is path to place where photos from flash drive will be copied
+* `copy_script` is path to `copy_files.sh` script
+
 ## How to enable service
 
 Set Your path to `button.py` script in `repo-dir/button/button.service`:
--- button/button.py
+++ button/button.py
@@ -6,12 +6,13 @@ import subprocess
 BUTTON_PIN = 23     # pin number in BCM standard
 mountpoint = "/mnt/flash"
 copydir = "/mnt/matrix/flash/"
+copy_script = "/home/pi/button/copy_files.sh"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)     # set BUTTON_PIN as input and configure pull up on it
 
 def do_flash(date, mountpoint, copydir):
-    bashCommand = "/home/pi/button/copy_files.sh {} {} {}".format(mountpoint, copydir, date)
+    bashCommand = "{} {} {} {}".format(copy_script, mountpoint, copydir, date)
     process = subprocess.check_call(bashCommand.split())
 
 def button_pressed(channel):
